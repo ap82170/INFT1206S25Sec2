f3d136a19f6aaa4a1ae576cf6dc2b668b4e1e76e
ap82170 <ap82170@gmail.com>
2026-07-08 19:30:49 -0400

    add comments to html and js file

--- Assignment4/Part2/script.js
+++ Assignment4/Part2/script.js
@@ -1,3 +1,9 @@
+/*
+    Name: Parth Dalwadi
+    File:script.js
+    Date:08 July, 2026
+    Description:This file contains letter which is the second part of assignment4.
+*/
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
