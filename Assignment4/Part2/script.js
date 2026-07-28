3787237b6c16cf6b1963d9828bd4762c03b3bcab
ap82170 <ap82170@gmail.com>
2026-07-28 14:00:33 -0400

    Update script.js

--- Assignment4/Part2/script.js
+++ Assignment4/Part2/script.js
@@ -2,7 +2,7 @@
     Name: Parth Dalwadi
     File:script.js
     Date:08 July, 2026
-    Description:This file contains letter which is the second part of assignment4.
+    Description:This file contains the image gallery for Part 2 of Assignment 4.
 */
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
@@ -10,7 +10,7 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
-// Create a data object
+// Store the image information
 
 const images = [
   { filename: "pic1.jpg", alt: "Closeup of a human eye" },
@@ -18,4 +18,9 @@ const images = [
   { filename: "pic3.jpg", alt: "Purple and white pansies" },
   { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
   { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+<<<<<<< Updated upstream
 ];
+=======
+];
+
+>>>>>>> Stashed changes
